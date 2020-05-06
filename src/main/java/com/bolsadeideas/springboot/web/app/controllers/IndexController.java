8112b25d5f89af55aa45f1805e26e814ef4e7bca
Ivan rl <ivanlaureano77@gmail.com>
2020-05-06 12:50:37 -0500

    se agrego clase model y vista perfil

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bolsadeideas.springboot.web.app.models.Usuarios;
+
 
 @Controller
 @RequestMapping("/app")
@@ -16,6 +18,19 @@ public class IndexController {
 		model.addAttribute("titulo", "Hola spring desde controlador");
 		
 		return "index";
+	}
+	
+	@RequestMapping("/perfil")
+	public String metodoTabla(Model model) {
+		Usuarios user=new Usuarios();
+		user.setNombre("Ivan");
+		user.setApellido("Ramos");
+		
+		model.addAttribute("user",user);
+		model.addAttribute("titulo", "Perfil del usuario---->".concat(user.getNombre()));
+			
+		
+		return "perfil";
 	}
 
 }
